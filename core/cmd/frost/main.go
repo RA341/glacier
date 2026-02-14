e8380cb78e253c99ca84345abc808f5be983b239
RA341 <razerishi1505@gmail.com>
2026-02-13 21:56:03 -0500

    add syso

--- core/cmd/frost/main.go
+++ core/cmd/frost/main.go
@@ -4,6 +4,7 @@ import (
 	"embed"
 
 	frost "github.com/ra341/glacier/frost/app"
+	_ "github.com/ra341/glacier/frost/app/syso" // needed for windows
 	"github.com/ra341/glacier/internal/app"
 	"github.com/ra341/glacier/internal/info"
 	"github.com/ra341/glacier/shared/api"
---
+++ core/frost/app/syso/gen.go
@@ -0,0 +1,3 @@
+package syso
+
+//go:generate go run github.com/akavel/rsrc@latest -manifest frost.exe.manifest -o res_windows.syso
